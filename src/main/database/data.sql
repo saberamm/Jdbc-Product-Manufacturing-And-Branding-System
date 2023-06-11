becc79973c03191462cb6fcdc19b8be33df427ef
saber <sbaerm.com@gmail.com>
2023-06-11 16:06:08 +0330

    create shareholder and product and shareholder_brand tables

--- src/main/database/data.sql
+++ src/main/database/data.sql
@@ -31,3 +31,28 @@ create table if not exists brand
 );
 alter table brand owner to postgres;
 ----------------------------
+create table if not exists product
+(
+    product_id serial primary key unique not null ,
+    name varchar(20) not null ,
+    create_date date not null ,
+    category_id integer not null constraint category_id references "category",
+    brand_id integer not null constraint brand_id references brand
+);
+alter table product owner to postgres;
+----------------------------
+create table if not exists shareholder
+(
+    shareholder_id serial primary key not null unique ,
+    name varchar(20) not null ,
+    phone_number varchar(11) not null ,
+    national_code varchar(20) unique not null
+);
+alter table shareholder owner to postgres;
+----------------------------
+create table if not exists shareholder_brand
+(
+    shareholder_id integer not null constraint shareholder_id references shareholder,
+    brand_id integer not null constraint brand_id references brand
+);
+alter table shareholder_brand owner to postgres;
