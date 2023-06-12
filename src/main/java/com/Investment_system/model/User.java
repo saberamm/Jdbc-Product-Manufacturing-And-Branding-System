3bb866a8598d7150b63c23820fd5bc3524c05f72
saber <sbaerm.com@gmail.com>
2023-06-12 16:28:35 +0330

    add user model

---
+++ src/main/java/com/Investment_system/model/User.java
@@ -0,0 +1,67 @@
+package com.Investment_system.model;
+
+public class User {
+    int user_id;
+    String name;
+    String user_name;
+    String email;
+    String password;
+
+    public User(String name, String user_name, String email, String password) {
+        this.name = name;
+        this.user_name = user_name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", name='" + name + '\'' +
+                ", user_name='" + user_name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
