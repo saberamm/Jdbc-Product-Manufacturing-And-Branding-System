b6d6f59dbfbe25950164b66529beada4ef6f9a66
saber <sbaerm.com@gmail.com>
2023-06-12 16:01:05 +0330

    add connection

---
+++ src/main/java/com/Investment_system/connection/DBConfig.java
@@ -0,0 +1,9 @@
+package com.Investment_system.connection;
+
+public class DBConfig {
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/Investment-system";
+    public static final String USER_NAME = "postgres";
+    public static final String PASSWORD = "56275627";
+
+}
