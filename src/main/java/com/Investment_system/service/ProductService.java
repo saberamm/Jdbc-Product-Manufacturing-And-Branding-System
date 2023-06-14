fa9bd64943a9fc1c4467a0d45ed3ecc1ff30eb6a
saber <sbaerm.com@gmail.com>
2023-06-14 15:23:22 +0330

    add product service interface

---
+++ src/main/java/com/Investment_system/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.Investment_system.service;
+
+import com.Investment_system.model.Product;
+
+public interface ProductService {
+    void add(Product product);
+    void delete(int id);
+    void update(Product product,int id);
+}
