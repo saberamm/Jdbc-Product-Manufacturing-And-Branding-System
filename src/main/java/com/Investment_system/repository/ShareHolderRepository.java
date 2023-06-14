b1811902a1026b0cefdcf4f59c7983b16c4ff712
saber <sbaerm.com@gmail.com>
2023-06-14 15:20:33 +0330

    add shareholder repository interface

---
+++ src/main/java/com/Investment_system/repository/ShareHolderRepository.java
@@ -0,0 +1,16 @@
+package com.Investment_system.repository;
+
+import com.Investment_system.model.Brand;
+import com.Investment_system.model.Shareholder;
+
+public interface ShareHolderRepository {
+
+    void add(Shareholder shareholder);
+    void delete(int id);
+    Shareholder load(int id);
+    Shareholder[] loadAll();
+    int rowCounter();
+    void update(Shareholder shareholder,int id);
+    boolean isShareholderExist();
+    boolean isAnyShareholderExist();
+}
