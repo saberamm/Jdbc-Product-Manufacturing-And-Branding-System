ec3ac2ca8b60025a986c79cb678872af0a91d135
saber <sbaerm.com@gmail.com>
2023-06-14 15:22:56 +0330

    add category service interface

---
+++ src/main/java/com/Investment_system/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.Investment_system.service;
+
+import com.Investment_system.model.Category;
+
+public interface CategoryService {
+    void add(Category category);
+    void delete(int id);
+    Category load(int id);
+    Category[] loadAll();
+    int rowCounter();
+    void update(Category category,int id);
+    boolean isCategoryExist();
+    boolean isAnyCategoryExist();
+}
