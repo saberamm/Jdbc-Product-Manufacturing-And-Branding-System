8ea4df4a6a711e2d50d1a2a735327f60e21a8583
saber <sbaerm.com@gmail.com>
2023-06-14 16:50:04 +0330

    add get services methods to application context

--- src/main/java/com/Investment_system/util/ApplicationContext.java
+++ src/main/java/com/Investment_system/util/ApplicationContext.java
@@ -1,15 +1,21 @@
 package com.Investment_system.util;
 
 import com.Investment_system.connection.DBConnection;
-import com.Investment_system.repository.UserRepository;
-import com.Investment_system.repository.impl.UserRepositoryImpl;
-import com.Investment_system.service.UserService;
-import com.Investment_system.service.impl.UserServiceImpl;
+import com.Investment_system.repository.*;
+import com.Investment_system.repository.impl.*;
+import com.Investment_system.service.*;
+import com.Investment_system.service.impl.*;
 
 import java.sql.Connection;
 
 public class ApplicationContext {
     private static Connection connection;
+
+    static {
+        connection = new DBConnection().getConnection();
+    }
+
+    //-------------user--------------------
     private static UserRepository userRepository;
     private static UserService userService;
 
@@ -18,9 +24,69 @@ public class ApplicationContext {
     }
 
     static {
-        connection = new DBConnection().getConnection();
         userRepository = new UserRepositoryImpl(connection);
         userService = new UserServiceImpl(userRepository);
     }
 
+    //---------------brand--------------------------------
+    private static BrandRepository brandRepository;
+    private static BrandService brandService;
+
+    public static BrandService getBrandService() {
+        return brandService;
+    }
+
+    static {
+        brandRepository = new BrandRepositoryImpl(connection);
+        brandService = new BrandServiceImpl(brandRepository);
+    }
+
+    //-----------------category-------------------------------
+    private static CategoryRepository categoryRepository;
+    private static CategoryService categoryService;
+
+    public static CategoryService getCategoryService() {
+        return categoryService;
+    }
+
+    static {
+        categoryRepository = new CategoryRepositoryImpl(connection);
+        categoryService = new CategoryServiceImpl(categoryRepository);
+    }
+    //------------------shareholder-----------------------------
+    private static ShareHolderRepository shareHolderRepository;
+    private static ShareHolderService shareHolderService;
+
+    public static ShareHolderService getShareHolderService() {
+        return shareHolderService;
+    }
+
+    static {
+        shareHolderRepository = new ShareHolderRepositoryImpl(connection);
+        shareHolderService = new ShareHolderServiceImpl(shareHolderRepository);
+    }
+    //--------------------product-------------------------------
+    private static ProductRepository productRepository;
+    private static ProductService productService;
+
+    public static ProductService getProductService() {
+        return productService;
+    }
+
+    static {
+        productRepository = new ProductRepositoryImpl(connection);
+        productService = new ProductServiceImpl(productRepository);
+    }
+    //-------------------shareholder-brand-------------------------
+    private static ShareHolderBrandRepository shareHolderBrandRepository;
+    private static ShareHolderBrandService shareHolderBrandService;
+
+    public static ShareHolderBrandService getShareHolderBrandService() {
+        return shareHolderBrandService;
+    }
+
+    static {
+        shareHolderBrandRepository = new ShareHolderBrandRepositoryImpl(connection);
+        shareHolderBrandService = new ShareHolderBrandServiceImpl(shareHolderBrandRepository);
+    }
 }
